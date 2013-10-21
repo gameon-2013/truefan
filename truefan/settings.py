e36ba09bc811abcaf951f89a716853c667dc49cb
caninemwenja <mwenja07@gmail.com>
2013-10-21 14:58:02 +0300

    added localhost to db settings

--- truefan/settings.py
+++ truefan/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'truefan',
         'PASSWORD': 'pass',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
