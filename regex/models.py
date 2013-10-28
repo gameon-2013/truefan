89ae48fb3f2f5d282958dde2110bd4cd77c7464e
caninemwenja <mwenja07@gmail.com>
2013-10-28 22:03:20 +0300

    removed CompiledRegex model, improved Keyword and added tests

--- regex/models.py
+++ regex/models.py
@@ -3,60 +3,25 @@ import re
 from django.db import models
 
 class Keyword(models.Model):
-    value = models.CharField(max_length=50)
+    value = models.CharField(max_length=50, unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kargs):
-        ''' adds a new compiled regex every time a keyword is saved '''
-        super(Keyword, self).save(*args,**kargs)
-
-        CompiledRegex.recompile()
-
-    def delete(self, *args, **kargs):
-        ''' adds a new compiled regex every time a keyword is removed '''
-        super(Keyword, self).delete(*args, **kargs)
-
-        CompiledRegex.recompile()
 
     def __unicode__(self):
         ''' shows the keywords value when printing the object '''
         return self.value
 
-class CompiledRegex(models.Model):
-    regex = models.TextField()
-    date_compiled = models.DateTimeField(auto_now=True)
-
-    def __get_compiled_regex(self):
-        return re.compile(self.regex, re.IGNORECASE)
-
-    value = property(__get_compiled_regex)
-
     @classmethod
-    def recompile(self):
-        ''' adds a new compiled regex '''
+    def regex(self):
+        ''' returns a regex OR of all the keywords '''
         keywords = [keyword.value for keyword in Keyword.objects.all()]
         regex = "|".join(keywords)
-
-        new_compiled_regex = CompiledRegex(regex=regex)
-        new_compiled_regex.save()
-
-    def __unicode__(self):
-        ''' shows the compiled regex's original regex when printing the object '''
-        return self.regex
-
-    @classmethod
-    def latest(self):
-        ''' returns the most recent compiled regex '''
-        recent = None
-        try:
-            recent = CompiledRegex.objects.order_by('-date_compiled')[0]
-        except IndexError:
-            pass
-
-        return recent
+        return regex
 
     @classmethod
     def match(self, sample):
-        ''' returns a list of words that match the regex in the sample '''
-        latest = CompiledRegex.latest()
-        return latest.value.findall(sample)
+        ''' returns a list of words that match the regex of all keywords 
+        against the sample '''
+        matcher = re.compile(Keyword.regex(), re.IGNORECASE)
+        # TODO cache matcher
+
+        return matcher.findall(sample)
--- regex/tests.py
+++ regex/tests.py
@@ -1,16 +1,46 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from models import *
 
+class KeywordModelTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_create_keyword(self):
+        k = Keyword()
+        self.assertNotEqual(k, None, "Can't create keyword")
+
+    def test_store_keyword(self):
+        k = Keyword(value='rugby')
+        k.save()
+
+        keywords = Keyword.objects.all();
+
+        self.assertEqual(len(keywords), 1, "Keyword not saved")
+        self.assertIn(k, keywords, "Keyword not in list of saved keywords")
+    
+    def test_keyword_uniqueness(self):
+        ''' fails if an error is not raised when saving 2 similar keywords '''
+        k = Keyword(value='ruj')
+        k.save()
+
+        k2 = Keyword(value='ruj')
+
+        self.assertRaises(IntegrityError, k2.save)
+
+class RegexTest(TestCase):
+    ''' test regex construct in keywords '''
+    
+    def setUp(self):
+        self.keywords = ['rugby', 'ruj', 'sevens']
+        for i in self.keywords:
+            Keyword.objects.create(value=i)
+
+    def test_regex(self):
+        test_rx = "|".join(self.keywords)
+        self.assertEqual(test_rx, Keyword.regex(), "Regex did not match")
+
+    def test_match(self):
+        sample = "This is rugby sevens"
+        results = Keyword.match(sample)
+
+        self.assertEqual(len(results), 2, "Wrong number of matches")
+        self.assertIn('rugby', results, "Rugby not one of the results of the match")
