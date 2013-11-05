e5c8a7a6e6dbe2da4d810ad714d7a8b29c38c0fb
caninemwenja <mwenja07@gmail.com>
2013-11-06 00:03:35 +0300

    added model methods for analyzing user tweets with tests

--- twyauth/models.py
+++ twyauth/models.py
@@ -1,6 +1,10 @@
+import twyauth
+
 from django.db import models
 from django.contrib.auth.models import User
+from twython import Twython
 
+from regex.models import Keyword
 
 class TwitterProfile(models.Model):
     """
@@ -11,3 +15,78 @@ class TwitterProfile(models.Model):
     twitter_userid = models.CharField(max_length=50)
     oauth_token = models.CharField(max_length=200)
     oauth_secret = models.CharField(max_length=200)
+    since_id = models.BigIntegerField()
+    
+    def get_api(self):
+        return Twython(twyauth.TWITTER_KEY, twyauth.TWITTER_SECRET,
+                      self.oauth_token, self.oauth_secret)
+    
+    def get_tweets(self, tweet_limit=twyauth.TWEET_LIMIT):
+        """ retrieves the latest tweets for a user, if it's their
+            first time, retrieves their last 200 tweets """
+        api = self.get_api()
+        tweets = []
+        
+        if self.since_id and self.since_id > 0:
+            since_id = self.since_id
+            req_tweets = api.get_user_timeline(since_id=since_id)
+            while len(req_tweets) > 0 and len(tweets) <= tweet_limit:
+                tweets = tweets + req_tweets
+                ids = [tweet['id'] for tweet in req_tweets]
+                since_id = max(ids)
+                req_tweets = api.get_user_timeline(since_id=since_id)
+        else:
+            last_id = None
+            req_tweets = api.get_user_timeline()
+            while len(req_tweets) > 0 and len(tweets) <= tweet_limit:
+                possible_last_id = req_tweets[-1]['id']
+                
+                if last_id:
+                    tweets = tweets + req_tweets[1:]
+                    
+                    if possible_last_id == last_id:
+                        break
+                else:
+                    tweets = tweets + req_tweets
+
+                last_id = possible_last_id
+                req_tweets = api.get_user_timeline(max_id=last_id)
+                
+        return tweets
+    
+    def analyze_last_tweets(self, tweet_limit=twyauth.TWEET_LIMIT):
+        """ get users last tweets and see if they are rugby tweets """
+        
+        tweets = self.get_tweets(tweet_limit)
+        
+        # store the latest id
+        ids = [tweet['id'] for tweet in tweets]
+        self.since_id = max(ids)
+        
+        # find rugby tweets
+        for tweet in tweets:
+            matches = Keyword.match(tweet['text'])
+            
+            if len(matches) > 0:
+                rugby_tweet = RugbyTweet()
+                rugby_tweet.text = tweet['text']
+                rugby_tweet.tweet_id = tweet['id']
+                rugby_tweet.created_at = tweet['created_at']
+                rugby_tweet.matches = ",".join(matches)
+                rugby_tweet.confidence = len(matches)
+                rugby_tweet.profile = self
+                
+                rugby_tweet.save()
+
+class RugbyTweet(models.Model):
+    """ stores tweets that are tagged as containing rugby 
+        words """
+    text = models.CharField(max_length=200)
+    tweet_id = models.BigIntegerField()
+    created_at = models.CharField(max_length=100, null=True) # TODO remove null
+    matches = models.CharField(max_length=200)
+    confidence = models.DecimalField(max_digits=6, decimal_places=4)
+    profile = models.ForeignKey('TwitterProfile', null=True) # TODO remove null
+    
+    def __str__(self):
+        return self.text + ": "+self.matches
--- twyauth/tests.py
+++ twyauth/tests.py
@@ -1,16 +1,88 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import twython
 
 from django.test import TestCase
+from mock import patch, Mock
 
+from regex.models import Keyword
+from models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+tweets = [
+    {
+        'id': 12500,
+        'text': 'its been awhile by Staind',
+        'created_at': None,
+    },
+    {
+        'id': 12470,
+        'text': 'safari sevens here we come',
+        'created_at': None,
+    },
+    {
+        'id': 12420,
+        'text': 'this is rugby nation',
+        'created_at': None,
+    },
+    {
+        'id': 12400,
+        'text': 'later',
+        'created_at': None,
+    },
+    {
+        'id': 12345,
+        'text': 'hello world',
+        'created_at': None,
+    },
+    
+]
+
+def custom_timeline(*args, **kargs):
+    if 'since_id' in kargs:
+        since_tweets = [tweet for tweet in tweets if tweet['id'] > kargs['since_id']]
+        return since_tweets
+    
+    if 'max_id' in kargs:
+        max_tweets = [tweet for tweet in tweets if tweet['id'] <= kargs['max_id']]
+        return max_tweets
+    
+    return tweets
+
+class TwitterProfileTest(TestCase):
+    """ tests for TwitterProfile model """
+    
+    def setUp(self):
+        self.patcher2 = patch('twython.Twython')
+        Twython_Mock = self.patcher2.start()
+        Twython_Mock.return_value.get_user_timeline.side_effect = custom_timeline
+        
+        self.patcher = patch.object(TwitterProfile, 'get_api', lambda obj: Twython_Mock())
+        
+        self.patcher.start()
+        
+        self.profile = TwitterProfile()    
+    
+    def tearDown(self):
+        self.patcher.stop()
+        self.patcher2.stop()
+    
+    def test_get_old_tweets(self):
+        """ test results gotten without since_id """
+        tweets = self.profile.get_tweets()
+        self.assertEqual(5, len(tweets), "Actual Tweet Results: %s" % repr(tweets))
+    
+    def test_get_latest_tweets(self):
+        """ test results gotten with since_id """
+        self.profile.since_id = 12420
+        tweets = self.profile.get_tweets()
+        self.assertEqual(2, len(tweets), "Actual Tweet Results: %s" % repr(tweets))
+    
+    def test_analyze_tweets(self):
+        """ tests for rugby tweets """
+        keywords = ['rugby', 'safari', 'sevens']
+        for k in keywords:
+            Keyword.objects.create(value=k)
+        
+        self.profile.analyze_last_tweets()
+        rugby_tweets = RugbyTweet.objects.all()
+        self.assertEqual(2, len(rugby_tweets), "Actual Rugby Tweets: %s" % repr(rugby_tweets))
+        self.assertEqual(12500, self.profile.since_id, "Actual Since ID: %i" % self.profile.since_id)
+
