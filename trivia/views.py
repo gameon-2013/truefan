a2ce814ba7f443796320cddd07c015a002561218
Chomba Ng'ang'a <choeringa@gmail.com>
2013-11-26 04:40:12 +0300

    Adding trivia fixtures, limiting trivia settings to valid users, changing timezone to nairobi

--- trivia/views.py
+++ trivia/views.py
@@ -1,9 +1,13 @@
 __author__ = 'mbacho'
 # Create your views here.
 
+from random import shuffle
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
 from models import Question
 from models import QuestionLevel
 from models import Choice
@@ -13,59 +17,64 @@ from forms import QuestionForm
 from forms import ChoiceForm
 from forms import ChoiceCategoryForm
 
-from random import shuffle
 
 def play(request, level=None):
     if level is None:
         levels = QuestionLevel.objects.all()
         return render_to_response('trivia/trivia.html', {'levels': levels})
 
+    selected_level = QuestionLevel.objects.get(name=level)
+    queries = list(Question.objects.filter(level=selected_level.id))
+    shuffle(queries)
+    query_choices = []
     try:
-        selected_level = QuestionLevel.objects.get(name=level)
-        queries = list(Question.objects.filter(level=selected_level.id))
-        shuffle(queries)
-        query_choices = []
         for i in queries:
-            choice_cats = list(Choice.objects.filter(category=i.correct_choice.category))
+            choice_cats = list(
+                Choice.objects.
+                filter(category=i.correct_choice.category).
+                exclude(id=i.correct_choice_id)[:3])
+            choice_cats.append(i.correct_choice)
             shuffle(choice_cats)
-            obj = { 'question' : i, 'choices': choice_cats }
+            obj = {'question': i, 'choices': choice_cats}
             query_choices.append(obj)
-        return render_to_response('trivia/trivia.html', {'level': level, 'questions': query_choices})
+        return render_to_response('trivia/trivia.html',
+                                  {'level': level, 'questions': query_choices, 'user': request.user})
     except Exception, ex:
         return render_to_response('error.html', {'error_message': str(ex)})
 
 
+@login_required(login_url='/')
 def questions(request):
-    if request.method == "POST":
-        frm = QuestionForm(request.POST)
-        if frm.is_valid():
-            q = Question(
-                content = frm.cleaned_data['content'],
-                correct_choice = frm.cleaned_data['correct_choice'],
-                level = frm.cleaned_data['level']
-            )
-            q.save()
+    frm = QuestionForm(request.POST or None)
 
-#        return HttpResponseRedirect(reverse('questions'))
+    if frm.is_valid():
+        q = Question(
+            content=frm.cleaned_data['content'],
+            correct_choice=frm.cleaned_data['correct_choice'],
+            level=frm.cleaned_data['level']
+        )
+        q.save()
+
+    #   return HttpResponseRedirect(reverse('questions'))
     queries = Question.objects.all()
     levels = QuestionLevel.objects.all()
     question_frm = QuestionForm()
     level_frm = QuestionLevelForm()
     return render_to_response('trivia/questions.html',
                               {'questions': queries, 'levels': levels, 'question_form': question_frm,
-                               'level_form': level_frm})
+                               'level_form': level_frm, 'user': request.user})
 
 
+@login_required(login_url='/')
 def choices(request):
-    if request.method == "POST":
-        frm = ChoiceForm(request.POST)
-        if frm.is_valid():
-            c = Choice(
-                content = frm.cleaned_data['content'],
-                category = frm.cleaned_data['category'],
-            )
-            c.save()
-#        return HttpResponseRedirect(reverse('choices'))
+    frm = ChoiceForm(request.POST or None)
+    if frm.is_valid():
+        c = Choice(
+            content=frm.cleaned_data['content'],
+            category=frm.cleaned_data['category'],
+        )
+        c.save()
+        #   return HttpResponseRedirect(reverse('choices'))
 
     choices = Choice.objects.all()
     categories = ChoiceCategory.objects.all()
@@ -73,25 +82,27 @@ def choices(request):
     cat_frm = ChoiceCategoryForm()
     return render_to_response('trivia/choices.html',
                               {'choices': choices, 'categories': categories, 'choice_form': choice_frm,
-                               'cat_form': cat_frm})
+                               'cat_form': cat_frm, 'user': request.user})
 
 
+@login_required(login_url='/')
 def question_level_save(request):
-    frm = QuestionLevelForm(request.POST)
+    frm = QuestionLevelForm(request.POST or None)
     if frm.is_valid():
         ql = QuestionLevel(
-            name = frm.cleaned_data['name'],
+            name=frm.cleaned_data['name'],
         )
         ql.save()
 
     return HttpResponseRedirect(reverse('questions'))
 
 
+@login_required(login_url='/')
 def choice_cat_save(request):
-    frm = ChoiceCategoryForm(request.POST)
+    frm = ChoiceCategoryForm(request.POST or None)
     if frm.is_valid():
         cc = ChoiceCategory(
-            name = frm.cleaned_data['name'],
+            name=frm.cleaned_data['name'],
         )
         cc.save()
 
